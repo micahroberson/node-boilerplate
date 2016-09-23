5c8f0648d9c1aaddd1564203edd4af6220779759
Micah Roberson <micah.roberson@gmail.com>
2016-09-23 16:40:32 -0700

    Update routes to wrap component and provide currentUser and appropriate.

--- src/common/components/routes.js
+++ src/common/components/routes.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
+import UserStore from '../stores/UserStore';
 import App from './App';
 import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+import Settings from './Settings';
 
-const routes = (
-  <Route path="/" component={App}>
-    <Route path="/sign-in" component={SignIn} />
-    <Route path="/sign-up" component={SignUp} />
-    <IndexRoute component={Home} />
-  </Route>
-);
+let routes = (componentContext) => {
+  let requireAuthentication = (nextState, replace) => {
+    if(!componentContext.getStore(UserStore).getCurrentUser()) {
+      replace('/sign-in');
+    }
+  };
+  let getWrapperComponent = (Handler) => {
+    return (props) => {
+      let currentUser = componentContext.getStore(UserStore).getCurrentUser();
+      return <Handler currentUser={currentUser} {...props} />;
+    }
+  };
+  return (
+    <Route path="/" component={App}>
+      <Route path="/sign-in" component={SignIn} />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/settings" component={getWrapperComponent(Settings)} onEnter={requireAuthentication}/>
+      <IndexRoute component={Home} />
+    </Route>
+  );
+}
 
 export default routes;
