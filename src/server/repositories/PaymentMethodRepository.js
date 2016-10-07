662ad3dca5e542a527c133004dc11d0bc2d7d15f
Micah Roberson <micah.roberson@gmail.com>
2016-10-07 10:22:53 -0700

    Add create method to PaymentMethodRepository which first creates a card via stripe.

--- src/server/repositories/PaymentMethodRepository.js
+++ src/server/repositories/PaymentMethodRepository.js
@@ -2,12 +2,38 @@ import _ from 'lodash';
 import Promise from 'bluebird';
 import BaseRepository from './BaseRepository';
 import PaymentMethod from '../../common/models/PaymentMethod';
+import {ParametersInvalidError} from '../lib/errors/APIError';
 
 class PaymentMethodsRepository extends BaseRepository {
   static tableName = 'payment_methods';
   static modelClass = PaymentMethod;
 
-  _serializePaymentMethodPlanForSql(paymentMethod) {
+  create(paymentMethod) {
+    if(!paymentMethod.stripe_token_id) {throw new Error('stripe_token_id must be provided');}
+    if(!paymentMethod.team) {throw new Error('team must be assigned');}
+    if(!paymentMethod.team.stripe_customer_id) {throw new Error('team must have a stripe_customer_id');}
+    return this.stripe.customers.createSource(
+      paymentMethod.team.stripe_customer_id,
+      {source: paymentMethod.stripe_token_id}
+    ).then((stripeCardObject) => {
+      delete paymentMethod.stripe_token_id;
+      paymentMethod.brand = stripeCardObject.brand;
+      paymentMethod.last_four = stripeCardObject.last4;
+      paymentMethod.expiration_month = stripeCardObject.exp_month;
+      paymentMethod.expiration_year = stripeCardObject.exp_year;
+      paymentMethod.stripe_card_id = stripeCardObject.id;
+      paymentMethod.stripe_card_object = stripeCardObject;
+      return super.create(paymentMethod);
+    })
+    .catch((error) => {
+      if(error.type === 'StripeCardError') {
+        throw new ParametersInvalidError({message: error.message});
+      }
+      throw error;
+    });
+  }
+
+  _serializePaymentMethodForSql(paymentMethod) {
     return {
       id: paymentMethod.id,
       team_id: paymentMethod.team_id,
